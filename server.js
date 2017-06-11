ce220961d8ca4e3d82c23dad0fc7979c74987c7a
Luna Kang <luna.kang.ca@gmail.com>
2017-06-11 07:43:32 -0700

    MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+var db
+
+MongoClient.connect('mongodb://lkang:lkang@ds119772.mlab.com:19772/star-wars-quotes', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
